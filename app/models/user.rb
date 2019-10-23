fa99d1690c17fa439f33c44debdc425232b53c76
curry333 <aiueoh123@gmail.com>
2019-10-23 15:20:58 +0000

    kadai-favorite

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,10 @@ class User < ApplicationRecord
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
+  has_many :favorites
+  has_many :favoritesposts, through: :favorites, source: :micropost
 
-def follow(other_user)
+  def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
@@ -29,5 +31,18 @@ def follow(other_user)
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+
+  def unlike(micropost)
+    favorite = favorites.find_by(micropost_id: micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def  favoritespost?(micropost)
+    self.favoritesposts.include?(micropost)
   end
 end
